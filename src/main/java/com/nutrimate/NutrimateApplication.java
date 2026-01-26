7653db4512e12e82601943b18c47eb2c6381c7bb
trungne08 <trungtdse170506@fpt.edu.vn>
2026-01-26 22:16:49 +0700

    start

---
+++ src/main/java/com/nutrimate/NutrimateApplication.java
@@ -0,0 +1,13 @@
+package com.nutrimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrimate/NutrimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
