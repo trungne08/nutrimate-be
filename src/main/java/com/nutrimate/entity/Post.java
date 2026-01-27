984a83c10a861fecb614e87c933189fee8e3a23a
Tanaka-2204 <khoabdse170432@fpt.edu.vn>
2026-01-28 02:40:01 +0700

    sửa lỗi vòng lặp ở bảng post

--- src/main/java/com/nutrimate/entity/Post.java
+++ src/main/java/com/nutrimate/entity/Post.java
@@ -1,8 +1,9 @@
 package com.nutrimate.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore; // 👉 Import dòng này
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.ToString; // Import thêm để tránh lỗi vòng lặp
+import lombok.ToString;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,7 @@ public class Post {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private User user; // Tác giả
+    private User user;
 
     @Column(columnDefinition = "TEXT")
     private String content;
@@ -31,20 +32,21 @@ public class Post {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt = LocalDateTime.now();
 
-    // Cache số lượng để query cho nhanh
     @Column(name = "like_count", nullable = false)
     private Integer likeCount = 0;
     
     @Column(name = "comment_count", nullable = false)
     private Integer commentCount = 0;
 
-    // 👇 THÊM ĐOẠN NÀY: Cấu hình Cascade để tự động xóa Comment & Like khi xóa Post
+    // 👇 THÊM @JsonIgnore VÀO 2 LIST NÀY ĐỂ SỬA LỖI SWAGGER 500
     
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    @ToString.Exclude // Quan trọng: Ngăn chặn lỗi StackOverflow do vòng lặp toString()
+    @ToString.Exclude
+    @JsonIgnore // 👈 Quan trọng: Ngắt vòng lặp JSON
     private List<Comment> comments = new ArrayList<>();
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    @ToString.Exclude // Quan trọng
+    @ToString.Exclude
+    @JsonIgnore // 👈 Quan trọng: Ngắt vòng lặp JSON
     private List<PostLike> likes = new ArrayList<>();
 }
