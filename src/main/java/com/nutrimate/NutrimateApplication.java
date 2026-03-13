4351aec7e6fd1212a9af91062d8375e9faff7870
trungne08 <trungtdse170506@fpt.edu.vn>
2026-03-13 19:08:09 +0700

    FIX LỖI CONFLICT TASK EXECUTOR CỦA @ASYNC VÀ WEBSOCKET

--- src/main/java/com/nutrimate/NutrimateApplication.java
+++ src/main/java/com/nutrimate/NutrimateApplication.java
@@ -3,12 +3,10 @@ package com.nutrimate;
 import com.nutrimate.config.DotEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
 public class NutrimateApplication {
 
 	public static void main(String[] args) {
